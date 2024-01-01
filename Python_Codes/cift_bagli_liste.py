d0fef898a3393799d784cd299aa2f55b366d679a
Muhammed Köle <muhammedkole18@gmail.com>
2024-01-01 12:55:57 +0300

    Bağlı liste üzerinde arayüz güncellemesi

--- Python_Codes/cift_bagli_liste.py
+++ Python_Codes/cift_bagli_liste.py
@@ -45,14 +45,14 @@ class BagliListe:
             self.head = bosluk2
             self.tail = bosluk2
             count = liste.Boyut()
-            Label(text=count, font="Arial 11").place(width=11, x=272, y=124)
+            Label(text=count, font="Arial 11").place(width=11, x=610, y=260)
     
         else:
             self.tail.next = bosluk2
             bosluk2.prev = self.tail
             self.tail = bosluk2
             count = liste.Boyut()
-            Label(text=count, font="Arial 11").place(width=11, x=272, y=124)
+            Label(text=count, font="Arial 11").place(width=11, x=610, y=260)
 
     def BasaEkle(self, data):
         if data == "":
@@ -63,14 +63,14 @@ class BagliListe:
             self.head = bosluk2
             self.tail = bosluk2
             count = liste.Boyut()
-            Label(text=count, font="Arial 11").place(width=11, x=272, y=124)
+            Label(text=count, font="Arial 11").place(width=11, x=610, y=260)
     
         else:
             bosluk2.next = self.head
             self.head.prev = bosluk2
             self.head = bosluk2
             count = liste.Boyut()
-            Label(text=count, font="Arial 11").place(width=11, x=272, y=124)
+            Label(text=count, font="Arial 11").place(width=11, x=610, y=260)
 
     def ArayaEkle(self, data, indeks):
         if data == "":
@@ -97,7 +97,7 @@ class BagliListe:
              self.head.prev = bosluk2
              self.head = bosluk2
              count = liste.Boyut()
-             Label(text=count, font="Arial 11").place(width=11, x=272, y=124)
+             Label(text=count, font="Arial 11").place(width=11, x=610, y=260)
 
         elif indeks == self.Boyut():
              bosluk2.prev = self.tail
@@ -105,7 +105,7 @@ class BagliListe:
              self.tail.next = bosluk2
              self.tail = bosluk2
              count = liste.Boyut()
-             Label(text=count, font="Arial 11").place(width=11, x=272, y=124)
+             Label(text=count, font="Arial 11").place(width=11, x=610, y=260)
             
         else:           
              bosluk3 = self.head
@@ -117,7 +117,7 @@ class BagliListe:
              bosluk3.next.prev = bosluk2
              bosluk3.next = bosluk2
              count = liste.Boyut()
-             Label(text=count, font="Arial 11").place(width=11, x=272, y=124)
+             Label(text=count, font="Arial 11").place(width=11, x=610, y=260)
 
     def AradanSil(self, indeks):
         if indeks == "":
@@ -126,19 +126,19 @@ class BagliListe:
         
         if self.BosMu():
             count = liste.Boyut()
-            Label(text=count, font="Arial 11").place(width=11, x=272, y=124)
+            Label(text=count, font="Arial 11").place(width=11, x=610, y=260)
             return
 
         if indeks == 0:
             self.BasiSil()
             count = liste.Boyut()
-            Label(text=count, font="Arial 11").place(width=11, x=272, y=124)
+            Label(text=count, font="Arial 11").place(width=11, x=610, y=260)
             return
         
         if indeks == self.Boyut() - 1: 
            self.SonuSil()
            count = liste.Boyut()
-           Label(text=count, font="Arial 11").place(width=11, x=272, y=124)
+           Label(text=count, font="Arial 11").place(width=11, x=610, y=260)
            return
         
         if int(indeks) >= liste.Boyut():
@@ -157,7 +157,7 @@ class BagliListe:
     def BasiSil(self):
         if self.BosMu():
             count = liste.Boyut()
-            Label(text=count, font="Arial 11").place(width=11, x=272, y=124)
+            Label(text=count, font="Arial 11").place(width=11, x=610, y=260)
             return
         
         temp = self.head
@@ -166,18 +166,18 @@ class BagliListe:
         if self.head is None:
             self.tail = None
             count = liste.Boyut()
-            Label(text=count, font="Arial 11").place(width=11, x=272, y=124)
+            Label(text=count, font="Arial 11").place(width=11, x=610, y=260)
     
         else:
             self.head.prev = None
             count = liste.Boyut()
-            Label(text=count, font="Arial 11").place(width=11, x=272, y=124)
+            Label(text=count, font="Arial 11").place(width=11, x=610, y=260)
             return temp.data                         
 
     def SonuSil(self):
         if self.BosMu():
             count = liste.Boyut()
-            Label(text=count, font="Arial 11").place(width=11, x=272, y=124)
+            Label(text=count, font="Arial 11").place(width=11, x=610, y=260)
             return
         
         temp = self.tail
@@ -186,12 +186,12 @@ class BagliListe:
         if self.tail is None:
             self.head = None
             count = liste.Boyut()
-            Label(text=count, font="Arial 11").place(width=11, x=272, y=124)
+            Label(text=count, font="Arial 11").place(width=11, x=610, y=260)
             
         else:
             self.tail.next = None
             count = liste.Boyut()
-            Label(text=count, font="Arial 11").place(width=11, x=272, y=124)
+            Label(text=count, font="Arial 11").place(width=11, x=610, y=260)
             return temp.data
 
     def Yazdir(self):
@@ -201,7 +201,7 @@ class BagliListe:
              messagebox.showerror("Hata", "Liste boş!")
          else:            
              box = Listbox(tk, font="Verdana 12 bold")
-             box.place(width=130, height=150, x=700)
+             box.place(width=130, height=200, x=450, y=280)
              i = 1
              bosluk = self.head
              while bosluk is not None: 
@@ -212,50 +212,69 @@ class BagliListe:
 liste = BagliListe()
 
 entry1 = Entry(tk, width=15)
-entry1.place(width=99, height=21, y=75, x=190)
+entry1.place(width=99, height=21, y=75, x=380)
 
 entry2 = Entry(tk, width=15)
-entry2.place(width=99, height=21, y=100, x=190)
+entry2.place(width=99, height=21, y=75, x=580)
 
-giris1 = Label(text="    Veri Gir:", font="Verdana 8 bold").place(width=80, x=110, y=75)
-giris2 = Label(text="İndeks Gir:", font="Verdana 8 bold").place(width=80, x=110, y=100)
-sayi = Label(text="Listedeki Eleman Sayısı:", font="Verdana 8 bold").place(width=160, x=110, y=125)
+giris1 = Label(text="    Veri Gir:", font="Verdana 8 bold").place(width=80, x=300, y=75)
+giris2 = Label(text="İndeks Gir:", font="Verdana 8 bold").place(width=80, x=500, y=75)
+sayi = Label(text="Listedeki Eleman Sayısı:", font="Verdana 8 bold").place(width=160, x=448, y=260)
 
 buton1 = Button(tk,
                 text="Başa Ekle", font="Verdana 12 bold",
                 padx=10, pady=0,
                 activebackground="green",
-                command=lambda: liste.BasaEkle(entry1.get())).place(width=100, height=60, x=0, y=0)
+                activeforeground="white",
+                foreground="white",
+                bg="green",
+                command=lambda: liste.BasaEkle(entry1.get())).place(width=100, height=60, x=150, y=110)
 
 buton2 = Button(tk, text="Sona Ekle", font="Verdana 12 bold",
                 padx=10, pady=0,
                 activebackground="green",
-                command=lambda: liste.SonaEkle(entry1.get())).place(width=100, height=60, x=0, y=70)
+                activeforeground="white",
+                foreground="white",
+                bg="green",
+                command=lambda: liste.SonaEkle(entry1.get())).place(width=100, height=60, x=260, y=110)
    
 buton3 = Button(tk, text="Araya Ekle", font="Verdana 12 bold",
                 padx=10, pady=0,
                 activebackground="green",
-                command=lambda: liste.ArayaEkle(entry1.get(), entry2.get())).place(width=100, height=60, x=0, y=140)
+                activeforeground="white",
+                foreground="white",
+                bg="green",
+                command=lambda: liste.ArayaEkle(entry1.get(), entry2.get())).place(width=100, height=60, x=370, y=110)
 
 buton4 = Button(tk, text="Başı Sil", font="Verdana 12 bold",
                 padx=10, pady=0,
-                activebackground="green",
-                command=lambda: liste.BasiSil()).place(width=100, height=60, x=0, y=210)
+                activebackground="red",
+                activeforeground="white",
+                foreground="white",
+                bg="red",
+                command=lambda: liste.BasiSil()).place(width=100, height=60, x=480, y=110)
 
 buton5 = Button(tk, text="Sonu Sil", font="Verdana 12 bold",
                 padx=10, pady=0,
-                activebackground="green",
-                command=lambda: liste.SonuSil()).place(width=100, height=60, x=0, y=280)
+                activebackground="red",
+                activeforeground="white",
+                foreground="white",
+                bg="red",
+                command=lambda: liste.SonuSil()).place(width=100, height=60, x=590, y=110)
 
 buton6 = Button(tk, text="Aradan Sil", font="Verdana 12 bold",
                 padx=10, pady=0,
-                activebackground="green",
-                command=lambda: liste.AradanSil(entry2.get())).place(width=100, height=60, x=0, y=350)
+                activebackground="red",
+                activeforeground="white",
+                foreground="white",
+                bg="red",
+                command=lambda: liste.AradanSil(entry2.get())).place(width=100, height=60, x=700, y=110)
 
 buton7 = Button(tk, text="Listeyi Göster", font="Verdana 12 bold",
                 padx=10, pady=0,
                 activebackground="green",
-                command=lambda: liste.Yazdir()).place(width=150, height=60, x=0, y=420)
+                activeforeground="white",
+                command=lambda: liste.Yazdir()).place(width=150, height=60, x=450, y=180)
 
 lbl = Label(tk)
 lbl.place(x=130, y=300)
