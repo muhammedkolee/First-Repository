129b43e98b8c540a72a2c7fe60beb55a5e413483
Muhammed Köle <muhammedkole18@gmail.com>
2024-01-31 21:22:11 +0300

    calculator.py tamamen değiştirildi

--- Python_Codes/calculator.py
+++ Python_Codes/calculator.py
@@ -1,381 +1,214 @@
-import re
-
-def calc(*args):
-    if len(args) == 2 or args[2] == None:
-        if type(args[1]) != int and type(args[1]) != float:
-            return f'Invalid number "{args[1]}"' 
-        if args[0] == "-" or args[0] == "sub":
-            return -args[1]
-        elif args[0] == "+" or args[0] == "add":
-            return args[1]
+def calc(operator = None, num1 = None, num2 = None):
+    if num2 == None:
+        if operator == '-' or operator == 'sub':
+            if type(num1) != int and type(num1) != float:
+                return f'Invalid number "{num1}"'
+            return -num1
+        elif operator == '+' or operator == 'add': 
+            if type(num1) != int and type(num1) != float:
+                return f'Invalid number "{num1}"'
+            return num1
         else:
-            return f'Invalid operator "{args[0]}"'       
-    elif len(args) == 3:
-        if type(args[1]) != int and type(args[1]) != float:
-            return f'Invalid number "{args[1]}"'
-        if type(args[2]) != int and type(args[2]) != float:
-            return f'Invalid number "{args[2]}"'
-        if args[0] == '+' or args[0] == "add":
-            return args[1] + args[2]
-        elif args[0] == '-' or args[0] == "sub":
-            return args[1]-args[2]
-        elif args[0] == '*' or args[0] == "mul":
-            return args[1] * args[2]
-        elif args[0] == '/' or args[0] == "div":
-            return args[1] / args[2]
-        elif args[0] == '%' or args[0] == "mod":
-            return args[1] % args[2]
-        elif args[0] == '^' or args[0] == "pow":
-            return args[1] ** args[2]
-        
-def eval(*args):
-    if isinstance(args[0], list):
-        if len(args[0]) == 3:
-            for i in range(3):
-                if isinstance(args[0][i], list):
-                    args[0][i] = eval(args[0][i])               
-            return calc(args[0][0], args[0][1], args[0][2])
-        elif len(args[0]) == 2:
-            for i in range(2):
-                if isinstance(args[0][i], list):
-                    args[0][i] = eval(args[0][i])
-            return calc(args[0][0], args[0][1])
-        else:
-            return f'Failed to evaluate "{args[0]}"'
+            return f'Invalid operator "{operator}"'
+    if type(num1) != int and type(num1) != float:
+        return f'Invalid number "{num1}"'
+    elif type(num2) != int and type(num2) != float:
+        return f'Invalid number "{num2}"'
+    
+    if operator == '+' or operator == 'add':
+        return num1+num2
+    elif operator == '-' or operator == 'sub':
+        return num1-num2
+    elif operator == '*' or operator == 'mul':
+        return num1*num2
+    elif operator == '/' or operator == 'div':
+        if num2 == 0:
+            return "Division by zero"
+        return num1/num2
+    elif operator == '%' or operator == 'mod':
+        if num2 == 0:
+            return "Division by zero"
+        return num1%num2
+    elif operator == '^' or operator == 'pow':
+        return num1**num2
     else:
-        return f'Failed to evaluate "{args[0]}"'
-
-def struct(arr):
-    if len(arr) == 2:
-        if arr[0] != str or chr:
-            arr[0], arr[1] = arr[1], arr[0]
-            return arr
+        return f'Invalid operator "{operator}"'
+    
+def eval(arr):
+    if type(arr) != list:
+        return f'Failed to evaluate "{arr}"'
+    elif len(arr) == 2:
+        if type(arr[1]) == list:
+            arr[1] = eval(arr[1])
+        return calc(arr[0], arr[1])
     elif len(arr) == 3:
-        if any(i in "(" for i in arr):
-            liste = list(arr[any(i in "(" for i in arr)])
-            del liste[0], liste[len(liste-1)]
-            arr[any(i in "(" for i in arr)] = struct(liste)
-        if arr[0] != str or chr:
+        if type(arr[1]) == list:
+            arr[1] = eval(arr[1])
+            eval(arr)
+        elif type(arr[2]) == list:
+            arr[2] = eval(arr[2])
+            eval(arr)
+        return calc(arr[0], arr[1], arr[2])
+    else:
+        return f'Failed to evaluate "{arr}"'
+    
+def struct(arr):
+    if type(arr) != list:
+        return f'Failed to structer "{arr}"'
+    
+    if len(arr) == 2:
+        if type(arr[0]) != str:
+            arr[0], arr[1] = arr[1], arr[0]
+        return arr
+    
+    if len(arr) % 2 == 0:
+        return f'Failed to structer "{arr}"'
+    
+    if '^' in arr or 'pow' in arr:
+        if '^' in arr:
+            num = arr.index('^')
+        elif 'pow' in arr:
+            num = arr.index('pow')
+        temp = []
+        temp.append(arr[num])
+        temp.append(arr[num-1])
+        temp.append(arr[num+1])
+        del arr[num-1], arr[num-1]
+        arr[num-1] = temp
+        struct(arr)
+
+    elif '%' in arr or 'mod' in arr:
+        if '%' in arr:
+            num = arr.index('%')
+        elif 'mod' in arr:
+            num = arr.index('mod')
+        temp = []
+        temp.append(arr[num])
+        temp.append(arr[num-1])
+        temp.append(arr[num+1])
+        del arr[num-1], arr[num-1]
+        arr[num-1] = temp
+        struct(arr)
+
+    elif '*' in arr or 'mul' in arr:
+        if '*' in arr:
+            num = arr.index('*')
+        elif 'mul' in arr:
+            num = arr.index('mul')
+        temp = []
+        temp.append(arr[num])
+        temp.append(arr[num-1])
+        temp.append(arr[num+1])
+        del arr[num-1], arr[num-1]
+        arr[num-1] = temp
+        struct(arr)
+
+    elif '/' in arr or 'div' in arr:
+        if '/' in arr:
+            num = arr.index('/')
+        elif 'div' in arr:
+            num = arr.index('div')
+        temp = []
+        temp.append(arr[num])
+        temp.append(arr[num-1])
+        temp.append(arr[num+1])
+        del arr[num-1], arr[num-1]
+        arr[num-1] = temp
+        struct(arr)
+    else:
+
+        if len(arr) > 3:
+            if '+' in arr or 'add' in arr or '-' in arr or 'sub' in arr:
+                    for i in range(1, len(arr), 2):
+                        if arr[i] == '+' or arr[i] == 'add':
+                            if arr[i] == '+':
+                                name = '+'
+                                break
+                            elif arr[i] == 'add':
+                                name = 'add'
+                                break 
+                        elif arr[i] == '-' or arr[i] == 'sub':
+                            if arr[i] == '-':
+                                name = '-'
+                                break
+                            elif arr[i] == 'sub':
+                                name = 'sub'
+                                break
+                    num = arr.index(name)
+                    temp = []
+                    temp.append(arr[num])
+                    temp.append(arr[num-1])
+                    temp.append(arr[num+1])
+                    del arr[num-1], arr[num]
+                    arr[num-1] = temp
+                    struct(arr)
+                    return arr    
+                        
+            temp = [i for i in arr if isinstance(i, str)]
+            mini = min(temp, key=len)
+            temp = []
+            temp.append(mini)
+            temp.append(arr[arr.index(mini)-1])
+            temp.append(arr[arr.index(mini)+1])
+            del arr[arr.index(mini)-1], arr[arr.index(mini)+1]
+            arr[arr.index(mini)] = temp
+            struct(arr)
+            return arr
+        
+    if len(arr) == 3:
+        if type(arr[0]) != str:
             arr[0], arr[1] = arr[1], arr[0]
             return arr
-    elif len(arr) % 2 != 0:
-        if any(i in "(" for i in arr):
-            liste = list(arr[any(i in "(" for i in arr)])
-            del liste[0], liste[len(liste-1)]
-            arr[any(i in "(" for i in arr)] = struct(liste)
-        elif "pow" in arr or '^' in arr:
-            if "pow" in arr: nick = "pow"
-            else: nick = '^'
-            dizi = [0]*3
-            num = arr.index(nick)
-            dizi[0] = arr[num]
-            dizi[1] = arr[num-1]
-            dizi[2] = arr[num+1]
-            del arr[num], arr[num]
-            arr[num-1] = dizi
-            struct(arr)
-
-            if type(arr[0]) != str and type(arr[0]) != chr:
-                dizi1 = [0]*3
-                dizi1[0] = arr[1]
-                dizi1[1] = arr[0]
-                dizi1[2] = arr[2]
-                arr[0] = dizi1[0]
-                arr[1] = dizi1[1]
-                arr[2] = dizi1[2]
-            return arr
         
-        elif "mul" in arr or '*' in arr:
-            if "mul" in arr: nick = "mul"
-            else: nick = '*'
-            dizi = [0]*3
-            num = arr.index(nick)
-            dizi[0] = arr[num]
-            dizi[1] = arr[num-1]
-            dizi[2] = arr[num+1]
-            del arr[num], arr[num]
-            arr[num-1] = dizi
-            struct(arr)
-
-            if type(arr[0]) != str and type(arr[0]) != chr:
-                dizi1 = [0]*3
-                dizi1[0] = arr[1]
-                dizi1[1] = arr[0]
-                dizi1[2] = arr[2]
-                arr[0] = dizi1[0]
-                arr[1] = dizi1[1]
-                arr[2] = dizi1[2]
-            return arr
-        
-        elif "div" in arr or '/' in arr:
-            if "div" in arr: nick = "div"
-            else: nick = '/'
-            dizi = [0]*3
-            num = arr.index(nick)
-            dizi[0] = arr[num]
-            dizi[1] = arr[num-1]
-            dizi[2] = arr[num+1]
-            del arr[num], arr[num]
-            arr[num-1] = dizi
-            struct(arr)
-
-            if type(arr[0]) != str and type(arr[0]) != chr:
-                dizi1 = [0]*3
-                dizi1[0] = arr[1]
-                dizi1[1] = arr[0]
-                dizi1[2] = arr[2]
-                arr[0] = dizi1[0]
-                arr[1] = dizi1[1]
-                arr[2] = dizi1[2]
-            return arr         
-            
-        elif "mod" in arr or '%' in arr:
-            if "mod" in arr: nick = "mod"
-            else: nick = '%'
-            dizi = [0]*3
-            num = arr.index(nick)
-            dizi[0] = arr[num]
-            dizi[1] = arr[num-1]
-            dizi[2] = arr[num+1]
-            del arr[num], arr[num]
-            arr[num-1] = dizi
-            struct(arr)
-
-            if type(arr[0]) != str and type(arr[0]) != chr:
-                dizi1 = [0]*3
-                dizi1[0] = arr[1]
-                dizi1[1] = arr[0]
-                dizi1[2] = arr[2]
-                arr[0] = dizi1[0]
-                arr[1] = dizi1[1]
-                arr[2] = dizi1[2]
-            return arr
-        else:
-            if len(arr[1]) < len(arr[3]):
-                dizi = [0]*3
-                dizi[0] = arr[1]
-                dizi[1] = arr[0]
-                dizi[2] = arr[2]
-                arr[0] = arr[3]
-                del arr[2], arr[2]
-                arr[1] = dizi
-                struct(arr)
-                if type(arr[0]) != str and type(arr[0]) != chr:
-                    dizi1 = [0]*3
-                    dizi1[0] = arr[1]
-                    dizi1[1] = arr[0]
-                    dizi1[2] = arr[2]
-                    arr[0] = dizi1[0]
-                    arr[1] = dizi1[1]
-                    arr[2] = dizi1[2]
-                return arr
-            else:
-                dizi = [0]*3
-                dizi[0] = arr[1]
-                dizi[1] = arr[0]
-                dizi[2] = arr[2]
-                del arr[1], arr[1]
-                arr[0] = dizi
-                struct(arr)
-                if type(arr[0]) != str and type(arr[0]) != chr:
-                    dizi1 = [0]*3
-                    dizi1[0] = arr[1]
-                    dizi1[1] = arr[0]
-                    dizi1[2] = arr[2]
-                    arr[0] = dizi1[0]
-                    arr[1] = dizi1[1]
-                    arr[2] = dizi1[2]
-                return arr
-
-def get_next(string):
-    if type(string) != list:
-        string = string.replace(" ", "")
-    arr = list(string)
-    for i in range(len(arr)):
-        if arr[i].isdigit():
-            arr[i] = int(arr[i])
-    i = 0
-    while i < len(arr):
-        if i+1 == len(arr):
-            return arr
-        if arr[i] == "(":
-            if arr[i+1] == "(":
-                j = i+1
-                dizi = []
-                while arr[j] != ")":
-                    dizi.append(arr[j])
-                    del arr[j]
-                dizi.append(")")
-                arr[i+1] = get_next(dizi)
-            while arr[i+1] != ")":
-                if arr[i+1] == "(":
-                    j = i+1
-                    dizi = []
-                    while arr[j] != ")":
-                        dizi.append(arr[j])
-                        del arr[j]
-                    dizi.append(")")
-                    del arr[j]
-                    arr[i+1] = get_next(dizi)
-                arr[i] = str(arr[i]) + str(arr[i+1])
-                del arr[i+1]
-            arr[i] = str(arr[i]) + str(arr[i+1])
-            del arr[i+1]
-            i += 1
-            continue
-        if arr[i+1] == "(":
-            j = i+1
-            while arr[j+1] != ")":
-                arr[i+1] = str(arr[i+1]) + str(arr[i+2])
-                del arr[i+2]
-            arr[i+1] = str(arr[i+1]) + str(arr[i+2])
-            del arr[i+2]
-            i += 2
-            continue
-        if type(arr[i]) == type(arr[i+1]):
-            arr[i] = str(arr[i]) + str(arr[i+1])
-            if arr[i].isdigit():
-                arr[i] = int(arr[i])
-            del arr[i+1]
-            continue
-        elif arr[i+1] == ".":
-            arr[i] = str(arr[i]) + str(arr[i+1])
-            del arr[i+1]
-            j = i+1
-            while j < len(arr):
-                if j+1 == len(arr):
-                    arr[i] = str(arr[i]) + str(arr[i+1])
-                    del arr[i+1]
-                    break
-                if type(arr[j]) == type(arr[j+1]):
-                    arr[j] = str(arr[j]) + str(arr[j+1])
-                    if arr[j].isdigit():
-                        arr[j] = int(arr[j])
-                    del arr[j+1]
-                    continue
-                else:
-                    arr[i] = str(arr[i]) + str(arr[i+1])
-                    del arr[i+1]
-                    arr[i] = float(arr[i])
-                    break
-            arr[i] = float(arr[i])
-            continue
-        i += 1
+    if len(arr) == 1:
+        if not arr[0][0]:
+            return f'Failed to structer "{arr}"'
+        if len(arr[0]) == 1:
+            return f'Failed to structer "{arr}"'
+        return arr[0]
+    if len(arr) < 3:
+        return arr[0]
     return arr
 
-def parse(string):
-    string = string.replace(" ", "")
-    return struct(get_next(string))
+print(struct([2, '?', 3])) #--> ['?', 2, 3]
+print(struct([3, 'test', 4.3, '+', 1])) #--> ['test', 3, ['+', 4.3, 1]]
+print(struct([1, 't', 3, '??', 5]))  #--> ['??', ['t', 1, 3], 5]
+print(struct([1, '+', 2, 'mol', 3, '+', 4])) # --> ['mol', ['+', 1, 2], ['+', 3, 4]]
 
+print("")
 
-print(get_next("(1 * (15 - 4) - 8) add 3 mul 2 sub (1 + 5)"))
+print(struct(['add', 5]))
 
+print("")
 
+print(struct([2, '-', 3]))                           #--> ['-', 2, 3]
+print(struct([1, '-', 3, '+', 2]))                   #--> ['+', ['-', 1, 3], 2]
+print(struct([1, '+', 2, '+', 3, '+', 4]))           #--> ['+', ['+', ['+', 1, 2], 3], 4]
+print(struct([1, 'sub', 2, 'add', 3, 'sub', 4]))     #--> ['sub', ['add', ['sub', 1, 2], 3], 4] ▓
 
+print("")
 
+print(struct([3, '*', 2]))                           #--> ['*', 3, 2]
+print(struct([1, '+', 2, '*', 3]))                   #--> ['+', 1, ['*', 2, 3]]
+print(struct([2, '%', 3, '-', 4, '/', 5.2]))         #--> ['-', ['%', 2, 3], ['/', 4, 5.2]]
+print(struct([1, 'add', 2, 'mod', 3, '-', 4]))       #--> ['-', ['add', 1, ['mod', 2, 3]], 4]   
+print(struct([5, '-', 5, 'mul', 5, 'div', 5]))       #--> ['-', 5, ['div', ['mul', 5, 5], 5]]
 
+print("")
 
+print(struct([2, '^', 3]))                           #--> ['^', 2, 3]
+print(struct([4, '*', 2, 'pow', 3]))                 #--> ['*', 4, ['pow', 2, 3]]
+print(struct([3, '^', 4, 'div', 2, 'pow', 3]))       #--> ['div', ['^', 3, 4], ['pow', 2, 3]]
 
+print("")
 
-# print(get_next("1+3", 2)) # +
-#        ["1", "2", "+", "3", "4"]
+print(struct(['-', 5]))             #--> ['-', 5]
+print(struct([2, '-', 5]))          #--> ['-', 2, 5]
+print(struct(['add', 5]))           #--> ['add', 5]
 
+print("")
 
-
-
-
-
-
-
-
-
-
-
-
-        # ["t", "e", "s", "t", "2", "3", "4", ".", "5"]
-        # ["t", "e", "s", "t", 2, 3, 4, ".", 5]
-        # ["test", "234.5"]
-
-# for i in range(len(arr)):
-#     if arr[i].isdigit():
-#         arr[i] = int(arr[i])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def get_next(string, index):
-#     arr = re.split("([+\-*/(add)])", string)
-#     arr = [i for i in arr if i != '']
-#     return arr[index]
-    
-
-
-
-
-
-
-
-
-
-
-
-
-# def get_next(string):
-#     arr = list(string)
-#     for i in range(len(arr)):
-#         if arr[i].isdigit():
-#             arr[i] = int(arr[i])
-#     i = 0
-#     while i < len(arr)-1:
-#         if type(arr[i]) == type(arr[i+1]):
-#             arr[i] = str(arr[i]) + str(arr[i+1])
-#             del arr[i+1]
-#             if arr[i].isdigit():
-#                 arr[i] = int(arr[i])
-#             i = 0
-#             continue
-#         elif type(arr[i]) == int and arr[i+1] == ".":
-#             arr[i] = str(arr[i]) + str(arr[i+1])
-#             del arr[i+1]
-#             if type(arr[i+1]) == int or arr[i+1].isdigit():
-#                 arr[i] = str(arr[i]) +str(arr[i+1])
-#                 del arr[i+1]
-#                 arr[i] = float(arr[i])
-#                 i = 0
-#                 continue
-#         i += 1
-#     return arr
-
-
-
-
-
-
-
-
-
-
-
-
-# struct([3, '*', 2])  ->  ['*', 3, 2]          
-# struct([1, '+', 2, 'mul', 3])  ->  ['+', 1, ['mul', 2, 3]]   
-# struct([2, 'mod', 3, '-', 4, '/', 5.2])  ->  ['-', ['mod', 2, 3], ['/', 4, 5.2]] 
-            
-# struct(4)  ->  Failed to structure "4"
-# struct(['+'])  ->  Failed to structure "['+']"
-# struct([2, '+', 3, '+'])  ->  Failed to structure "[2, '+', 3, '+']"
-            
-# struct([2, '?', 3])  ->  ['?', 2, 3]
-
-
-
+print(struct(4))                    #--> Failed to structure "4"
+print(struct(['+']))                #--> Failed to structure "['+']"                   
+print(struct([2, '+', 3, '+']))     #--> Failed to structure "[2, '+', 3, '+']"        
+print(struct([2, 'mul', '+']))      #--> ['mul', 2, '+']
